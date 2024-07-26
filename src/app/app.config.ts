b1d5fd77b99e0a78638d092a2e31bb455f360908
Bruno Cecilio <49068550+brunocecilio@users.noreply.github.com>
2024-07-25 23:09:11 -0300

    feat: adiciona configuração PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,31 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
 import { HttpClientModule } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   
   providers: [
     provideRouter(routes),
     {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        autoLogin: false,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider('328824258579-8173qq42qp7fmq156r2nr46rqbd63gk7.apps.googleusercontent.com'),
-          },
-        ],
-      } as SocialAuthServiceConfig,
-    },  
-    importProvidersFrom(HttpClientModule),      
-    
-  ]
+        provide: 'SocialAuthServiceConfig',
+        useValue: {
+            autoLogin: false,
+            providers: [
+                {
+                    id: GoogleLoginProvider.PROVIDER_ID,
+                    provider: new GoogleLoginProvider('328824258579-8173qq42qp7fmq156r2nr46rqbd63gk7.apps.googleusercontent.com'),
+                },
+            ],
+        } as SocialAuthServiceConfig,
+    },
+    importProvidersFrom(HttpClientModule),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
